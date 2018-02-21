d84eb1362b51013229a4181612639d957d5dba27
Dasha Fomina <df1732@nyu.edu>
2018-02-20 19:20:12 -0500

    Why

---
+++ DreeamWalk/Assets/Resources/Sprites/CountDown.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Collections;
+ 
+public class CountDown : MonoBehaviour {
+	public float timeRemaining = 60f;
+	// Use this for initialization
+	void Start () {
+ 
+	}
+	
+	void Update () {
+		if (timeRemaining > 0) {
+			Debug.Log("Waitting..."+timeRemaining);
+			timeRemaining -= Time.deltaTime;
+			if ( timeRemaining == 0 ) { GameOver(); }
+			} 
+	}
+ 
+	void GameOver(){
+ 
+	}
+}
---
+++ DreeamWalk/Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+	void start()
+	{
+		InitializeInventory();
+	}
+
+
+	void InitializeInventory() // why is it not changing once i press "play"?
+	{
+		var slots = GameObject.Find("Slots");
+		foreach(Transform slot in slots.transform)
+		{
+			slot.transform.GetChild(0).GetComponent<Image>().sprite = 
+				Resources.Load<Sprite>("Inventory Items/empty_item");
+		}
+	}
+}
